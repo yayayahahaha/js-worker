4331bb1133319abc2d8e470418397eb25e572e8d
flyc <yayayahahahaooii@gmail.com>
2018-11-09 16:09:14 +0800

    prune codes

--- worker.js
+++ worker.js
@@ -5,7 +5,4 @@ worker.onmessage = function(e) {
     console.log(`message from main.js: ${ e.data }`);
 
     worker.postMessage('I got it, please wait');
-    return;
-    // 如果關了，node 的程式也會結束
-    worker.close();
 }
