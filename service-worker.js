9242fed44d7467ba40d8eb90f0c6c8e494d67ba5
flyc <yayayahahahaooii@gmail.com>
2018-11-12 13:36:21 +0800

    format

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
-var cacheName = 'v15';
+var cacheName = 'v2';
 
 self.addEventListener('install', (event) => {
     console.log('service-worker installing');
-    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll([])));
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            cache.addAll([]);
+        }));
 })
 
 self.addEventListener('activate', event => {
@@ -12,13 +16,12 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     console.log(`handling fetch event for ${event.request.url}`);
 
-    return;
-    event.respondWith( caches.match(event.request).then((response) => {
+    event.respondWith(caches.match(event.request).then((response) => {
         if (response) {
-            console.log('曾經被快取存過!' + cacheName);
+            console.log(cacheName + '曾經被快取存過!');
             return response;
         } else {
-            console.log('沒有在快取裡面!' + cacheName);
+            console.log(cacheName + '沒有在快取裡面!');
             return fetch(event.request).then((response) => {
                 return caches.open(cacheName).then((cache) => {
                     cache.put(event.request, response.clone());
@@ -29,5 +32,5 @@ self.addEventListener('fetch', event => {
     }).catch((error) => {
         console.error('發生其他錯誤啦');
         console.error(error);
-    }) );
+    }));
 });
