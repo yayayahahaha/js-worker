dd2a2f215ded3178bc81a304e44c32043f0b8909
flyc <yayayahahahaooii@gmail.com>
2018-11-13 15:48:16 +0800

    test fucntion calling with Blob constructor works

--- main.js
+++ main.js
@@ -2,24 +2,51 @@
  web worker testing
  ******************/
 
-function webWorker() {
-/*
-    // 用來測試thread 與否的差異檔
-    for (var i = 0; i < 1000000000; i++) {
-        if (i % 100000000 === 0) {
-            console.log(i);
-        }
-    }
-    return;
-*/
+function sayHi() {
+    var worker = this;
 
+    worker.onmessage = function(e) {
+        console.log('got message!!');
+        console.log(`message from main.js: ${ e.data }`);
+
+        for (var i = 0; i < 1000000000; i++) {
+            if (i % 100000000 === 0) {
+                console.log(i);
+            }
+        }
+
+        worker.postMessage('I got it, please wait');
+    }
+}
+
+function run(fn) {
+    fn = fn ? fn : Function.prototype;
+    return new Worker(URL.createObjectURL(new Blob(['(' + fn + ')()'])));
+}
+
+function webWorker() {
     // dedicated workder, 單一 workder
-    var hardWorker = new Worker('./web-worker.js');
+
+    // 一般的靜態檔寫法
+    // var hardWorker = new Worker('./web-worker.js');
+    // 動態的function 寫法
+    var hardWorker = run(sayHi);
+
     hardWorker.postMessage('hi, there');
     hardWorker.onmessage = function(e) {
         console.log('I got message');
         console.log(e.data);
     }
+
+    /*
+        // 用來測試thread 與否的差異檔
+        for (var i = 0; i < 1000000000; i++) {
+            if (i % 100000000 === 0) {
+                console.log(i);
+            }
+        }
+        return;
+    */
 }
 
 
